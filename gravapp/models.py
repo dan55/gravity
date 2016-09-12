523b1cb7dc5922262649002c01dd58eba3d26aaf
Dan <dan.elkin85@gmail.com>
2016-09-11 20:00:30 -0400

    wiring up

--- gravapp/models.py
+++ gravapp/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Character(models.Model):
+    first_name = models.CharField(max_length=48)
+    last_name = models.CharField(max_length=48)
+
+class Relationship(models.Model):
+    character_1 = models.ForeignKey('Character', related_name='first_character')
+    character_2 = models.ForeignKey('Character', related_name='second_character')
--- gravapp/views.py
+++ gravapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from gravapp.models import Character
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    characters = Character.objects.all()
+    nodes, links = [], []
+
+    for character in characters:
+        nodes.append({ 'name': character.first_name })
+
+    context = { 'nodes': nodes, 'links': links }
+
+    return render(request, 'index.html', { 'context': context })
