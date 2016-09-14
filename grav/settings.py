d4851fb4157b5e3ca41777cd835f47d62e890370
Dan <dan.elkin85@gmail.com>
2016-09-13 20:06:55 -0400

    heroku db settings

--- grav/settings.py
+++ grav/settings.py
@@ -74,16 +74,10 @@ WSGI_APPLICATION = 'grav.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'grav',
-        'USER': 'gravuser',
-        'PASSWORD': 'gravpass',
-        'HOST': 'localhost',
-    }
-}
-
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
