d652a1803c19ca608ac9e6dbfe1f747599c2a386
Dan <dan.elkin85@gmail.com>
2016-09-15 20:37:48 -0400

    add ability to delete characters

--- gravapp/forms.py
+++ gravapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CharacterForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=48)
-    last_name = forms.CharField(label='Last Name', max_length=48)
+    last_name = forms.CharField(label='Last Name', max_length=48, required=False)
